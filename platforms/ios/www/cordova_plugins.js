d1d43a937af5bc1a83a883b085edcba3a3341ae6
McSam <mcsam1216@gmail.com>
2020-12-28 09:34:11 +0800

    gitignore fix

--- platforms/ios/www/cordova_plugins.js
+++
@@ -1,7 +0,0 @@
-cordova.define('cordova/plugin_list', function (require, exports, module) {
-    module.exports = [];
-    module.exports.metadata = {
-        'cordova-plugin-webpack': '0.4.4',
-        'cordova-plugin-whitelist': '1.3.4',
-    };
-});
